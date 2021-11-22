8058d5cc0f3826b1ee38ba3a74159969f928d40f
Zuradel <truongbien.qh@gmail.com>
2021-11-23 00:51:26 +0700

    test

--- Webdaugia/Webdaugia/Models/Lot.cs
+++ Webdaugia/Webdaugia/Models/Lot.cs
@@ -20,41 +20,44 @@
         public int ID { get; set; }
 
         [StringLength(200)]
-        [Required(ErrorMessage = "Bạn cần nhập cmnd")]
+        [Required(ErrorMessage = "Bạn cần nhập tên phiên đấu giá")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Bạn cần nhập cmnd")]
+        [Required(ErrorMessage = "Bạn  cần nhập bước giá")]
         public long? MiniumBid { get; set; }
-        [Required(ErrorMessage = "Bạn cần nhập cmnd")]
+        [Required(ErrorMessage = "Bạn cần nhập giá khởi điểm")]
         public long? StartingPrice { get; set; }
-        [Required(ErrorMessage = "Bạn cần nhập cmnd")]
+        [Required(ErrorMessage = "Bạn cần nhập tiền cọc")]
         public long? AdvanceDesposit { get; set; }
-        [Required(ErrorMessage = "Bạn cần nhập cmnd")]
+        [Required(ErrorMessage = "Bạn cần nhập phí tham gia")]
         public int? ParticipationFee { get; set; }
-        [Required(ErrorMessage = "Bạn cần nhập cmnd")]
+
         public int? HostLot { get; set; }
-        [Required(ErrorMessage = "Bạn cần nhập cmnd")]
+        [Required(ErrorMessage = "Bạn cần nhập ngày kết thúc đăng ký")]
         public DateTime? TimeForRegisterEnd { get; set; }
-        [Required(ErrorMessage = "Bạn cần nhập cmnd")]
+        [Required(ErrorMessage = "Bạn cần nhập ngày bắt đầu đấu giá")]
         public DateTime? TimeForBidEnd { get; set; }
-        [Required(ErrorMessage = "Bạn cần nhập cmnd")]
+        [Required(ErrorMessage = "Bạn cần nhập ngày kết thúc đấu giá")]
         public DateTime? TimeForBidStart { get; set; }
-        [Required(ErrorMessage = "Bạn cần nhập cmnd")]
+        [Required(ErrorMessage = "Bạn cần nhập ngày bắt đầu đăng ký")]
         public DateTime? TimeForRegisterStart { get; set; }
 
         public int? AssetID { get; set; }
 
         public bool? Status { get; set; }
-
+        [Required(ErrorMessage = "Vùi lòng chọn thể loại đấu giá")]
         public int? CateID { get; set; }
 
 
         [StringLength(200)]
+        [Required(ErrorMessage = "Vui lòng nhập chủ tài sản")]
         public string HostName { get; set; }
 
         [StringLength(158)]
+        [Required(ErrorMessage = "Vui lòng nhập địa chỉ")]
         public string Location { get; set; }
 
         [StringLength(158)]
+        [Required(ErrorMessage = "Vui lòng nhập thời gian xem sản phẩm")]
         public string ViewInTime { get; set; }
 
         [StringLength(128)]
@@ -77,6 +80,6 @@
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<RegisterBid> RegisterBids { get; set; }
-        
+        public List<Category> ListCategory = new List<Category>();
     }
 }
