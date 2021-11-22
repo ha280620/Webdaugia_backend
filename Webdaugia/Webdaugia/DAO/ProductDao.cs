d2bd23ba842a8132a149b7b717af3ffd0931fc57
turnturn74 <nlquan1363@gmail.com>
2021-11-22 16:37:06 +0700

    List/Create/DeleteProduct

--- Webdaugia/Webdaugia/DAO/ProductDao.cs
+++ Webdaugia/Webdaugia/DAO/ProductDao.cs
@@ -20,14 +20,14 @@ namespace Webdaugia.DAO
             return db.Lots.Find(id);
         }
 
-        public IEnumerable<Lot> ListAllPaging(string searchString, int page, int pageSize)
+        public IEnumerable<Product> ListAllPaging(string searchString, int page, int pageSize)
         {
-            IQueryable<Lot> model = db.Lots;
+            IQueryable<Product> model = db.Products;
             if (!string.IsNullOrEmpty(searchString))
             {
-                model = model.Where(x => x.Name.Contains(searchString) || x.Category.Name.Contains(searchString));
+                model = model.Where(x => x.Name.Contains(searchString) || x.Lot.Name.Contains(searchString) || x.Lot.ID.ToString().Contains(searchString));
             }
-            return model.OrderByDescending(x => x.TimeForBidEnd).ToPagedList(page, pageSize);
+            return model.OrderByDescending(x => x.CreatedAt).ToPagedList(page, pageSize);
         }
     }
 }
