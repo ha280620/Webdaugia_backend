7ace82f5964fc383ef61d65e595a7b39fd9a3946
Zuradel <truongbien.qh@gmail.com>
2021-11-30 16:29:56 +0700

    up

--- Webdaugia/Webdaugia/Models/Login/LoginModel.cs
+++ Webdaugia/Webdaugia/Models/Login/LoginModel.cs
@@ -9,8 +9,9 @@ namespace Webdaugia.Models.Login
     public class LoginModel
     {
         [Required(ErrorMessage = "Vui lòng nhập tên tài khoản")]
+        [StringLength(16, MinimumLength = 5, ErrorMessage = "Tài khoản có ít nhất 5 và nhỏ hơn 16 kí tự!")]
         public string Username { set; get; }
-        [Required(ErrorMessage = "Vui lòng nhập tên tài khoản")]
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
         public string Password { set; get; }
 
     }
